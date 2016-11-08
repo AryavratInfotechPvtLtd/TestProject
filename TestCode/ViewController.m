ad45cda97cde36273c97d45dc8187a5569358bfa
Rohit Parihar <user@users-MacBook-Pro.local>
2016-11-08 15:32:00 +0530

    08Novomber2016(03:31)

--- TestCode/ViewController.m
+++ TestCode/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self GetStringWithKey:@"developers"]
+    [self GetStringWithKey:@"developers"];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
